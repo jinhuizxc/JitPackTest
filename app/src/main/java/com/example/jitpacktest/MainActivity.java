b136d3f39837a5ece7d605bb27baa53b0a2db763
j_h <13113017500>
2019-08-30 09:52:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jitpacktest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jitpacktest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 实现自己的开源库----JitPack使用体验
+ * https://www.jianshu.com/p/9218d34fb8fa?utm_campaign=haruki&utm_content=note&utm_medium=reader_share&utm_source=weixin
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
